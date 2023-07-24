eeff2bd3d48f3fc51df04a9b4e921c5389b3e841
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-07-24 11:52:36 -0600

    Modificacion ligera

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import sys
 from framebuffer import FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, Color, framebuffer, point
 from render import renderBuffer
-CELL_SIZE = 4
+CELL_SIZE = 5
 
 WINDOW_WIDTH, WINDOW_HEIGHT = FRAMEBUFFER_WIDTH * CELL_SIZE, FRAMEBUFFER_HEIGHT * CELL_SIZE
 
@@ -14,7 +14,63 @@ def initialize_game():
             # Randomly set cells as alive (white) or dead (black)
             if random.random() < 0.3:
                 point(x, y)
+                    # Add still lifes
 
+    # Add still lifes
+    # Block
+    point(10, 10)
+    point(10, 11)
+    point(11, 10)
+    point(11, 11)
+
+    # Beehive
+    point(20, 10)
+    point(21, 9)
+    point(21, 11)
+    point(22, 9)
+    point(22, 11)
+    point(23, 10)
+
+    # Loaf
+    point(30, 10)
+    point(31, 9)
+    point(31, 11)
+    point(32, 9)
+    point(32, 12)
+    point(33, 11)
+    point(34, 10)
+
+    # Add oscillators
+    # Blinker
+    point(5, 20)
+    point(5, 21)
+    point(5, 22)
+
+    # Toad
+    point(15, 20)
+    point(15, 21)
+    point(15, 22)
+    point(16, 19)
+    point(16, 20)
+    point(16, 21)
+
+    # Beacon
+    point(25, 20)
+    point(25, 21)
+    point(26, 20)
+    point(26, 21)
+    point(27, 22)
+    point(28, 22)
+    point(27, 23)
+    point(28, 23)
+
+    # Add spaceships
+    # Glider
+    point(5, 35)
+    point(6, 36)
+    point(6, 37)
+    point(5, 37)
+    point(4, 36)
 def update_game():
     new_framebuffer = [[Color(0, 0, 0) for _ in range(FRAMEBUFFER_WIDTH)] for _ in range(FRAMEBUFFER_HEIGHT)]
 
